f3c88ba59486153b313d1029cc0736ae7b816736
mahalakshme <mahalakshme1994@gmail.com>
2015-01-10 23:54:13 +0530

    mahalakshme:First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+class Asterisk
+{
+  void print()
+  {
+      System.out.print("*");
+  }
+}
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Asterisk sample = new Asterisk();
+        sample.print();
+    }
+}
